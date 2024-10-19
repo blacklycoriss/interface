526f9b0808b58d2a792bd119eda9d7158998d4a5
BlackLycoriss <prokopovaleksandr544@gmail.com>
2024-10-19 12:51:06 +0700

    init project

---
+++ app/src/main/java/com/lycoriss/myfirstapp/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.lycoriss.myfirstapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.lycoriss.myfirstapp.ui.theme.MyFirstAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyFirstAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyFirstAppTheme {
+        Box {
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_background),
+                contentDescription = "Image in Row",
+                modifier = Modifier.size(45.dp)
+            )
+            Icon(
+                Icons.Filled.Check,
+                contentDescription = "Check mark"
+            )
+        }
+    }
+}
