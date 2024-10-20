2022ef145047dc99d22a1be14ff26450870b9a01
BlackLycoriss <prokopovaleksandr544@gmail.com>
2024-10-20 21:36:49 +0700

    clear note activity

--- app/src/main/java/com/lycoriss/myfirstapp/NoteActivity.kt
+++ app/src/main/java/com/lycoriss/myfirstapp/NoteActivity.kt
@@ -19,29 +19,11 @@ class NoteActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyFirstAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyFirstAppTheme {
-        Greeting("Android")
-    }
-}
+
